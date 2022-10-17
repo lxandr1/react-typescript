99ca1ba6bf3c276fe463f517f935fa522fccd7c0
alessandro1334 <sandroales405@gmail.com>
2022-10-17 16:01:49 +0700

    Revert ":white_check_mark: Initial commit"
    This reverts commit 226d854cfa96eb051d27c5c53ca39b95d55237b9.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { Button } from "./components/Button";
+import { Greet } from "./components/Greet";
+import { Heading } from "./components/Heading";
+import { Oscar } from "./components/Oscar";
+import { Status } from "./components/Status";
 
 function App() {
   return (
     <div className="App">
-      <Button
-        handleClick={(event, id) => {
-          console.log("Button click!", event, id);
-        }}
-      />
+      <Status status="success" />
+      {/* Parsing react props into child component */}
+      <Oscar>
+        <Heading>Oscar Dilahoya</Heading>
+      </Oscar>
+
+      {/* Make optional props */}
+      <Greet name="Alexandr" isLoggedIn={true} />
     </div>
   );
 }
